1af1d6cb3d2f45f79bab8f2008c5a6fafff9e209
Andrew Trice <triceam@gmail.com>
2022-03-01 09:25:02 -0500

    Revert "added output var"

--- outputs.tf
+++
@@ -1,5 +0,0 @@
-output "default_rwx_storage_class" {
-  description = "Default read-write-many storage class"
-  value       = "portworx-rwx-gp2-sc"
-  depends_on  = [ibm_resource_instance.portworx]
-}
